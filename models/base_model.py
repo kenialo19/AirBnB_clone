0c3848b88642e4342c577f394d2145c6a7ea8d88
eylens <eymasac@gmail.com>
2021-11-13 18:20:11 -0500

    line: 36 adjusted with blank space

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
 
     def __str__(self):
         """Prints representation of the BaseModel class."""
-        return "[{}]({}) {}".\
+        return "[{}] ({}) {}".\
             format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
