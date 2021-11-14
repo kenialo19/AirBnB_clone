c2c4639bfaafbeee30bcfe1d3d87be8106f442ec
kenialo19 <3396@holbertonschool.com>
2021-11-14 14:29:20 -0500

    modified file

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,10 @@
 
 import uuid
 from datetime import datetime
-import models
+from models import storage
+
+
+date_time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
@@ -16,7 +19,6 @@ class BaseModel:
         where *args won’t be used, kwargs is not empty, otherwise
         create id and created_at (new instance)"""
 
-        date_time = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs and kwargs != {}:
             for key, value in kwargs.items():
                 if key == "created_at":
@@ -30,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """Prints representation of the BaseModel class."""
@@ -40,7 +42,7 @@ class BaseModel:
     def save(self):
         """Updates the public instance attribute with the current datetime."""
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """Returns a dict. containing all keys/values of the instance."""
