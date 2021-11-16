3de2716f288656197618fe11aa3b1b82f23535d3
eylens <eymasac@gmail.com>
2021-11-16 06:18:15 -0500

    tests for state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Unittest for State module."""
+import unittest
+from models.state import State
+from models.base_model import BaseModel
+
+
+class TestPlace(unittest.TestCase):
+    """This class tests state module."""
+
+    def setUp(self):
+        """Sets up the testing environment"""
+
+        self.state_1 = State()
+        self.state_2 = State()
+        self.state_2.name = "Antioquia"
+
+    def test_new_instance(self):
+        """Checks that new instance was created."""
+
+        self.assertTrue(self.state_1)
+        self.assertTrue(self.state_2)
+
+    def test_inheritance(self):
+        """Review inherits from BaseModel."""
+
+        self.assertTrue(issubclass(State, BaseModel))
